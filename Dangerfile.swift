f7dfcffdae3b9638cd409ca70ab3c50616692f25
guillaume <guillaume.duez.officiel@gmail.com>
2024-06-10 19:19:52 +0200

    add example in dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -8,3 +8,7 @@ let report = XCodeSummary(filePath: "result.json")
 report.report()
 
 Coverage.xcodeBuildCoverage(.derivedDataFolder("build"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
+
+/*
+ Coverage.xcodeBuildCoverage(.xcresultBundle("/Users/guillaumeduez/Library/Developer/Xcode/DerivedData/danger-swift-coverage-gjqtkbcwtsyodtgrnwkbedvaavvd/Logs/Test/Test-DangerSwiftCoverage-2024.06.10_18-40-25-+0200.xcresult"), minimumCoverage: 99, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
+ */
